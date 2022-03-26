9082c9c91f2a6a6d3f778948cebe0801af0d2bec
KSAH-42 <63054588+KSAH-42@users.noreply.github.com>
2022-03-26 22:59:19 +0100

    Update BackgroundThread.h

--- VideoProvider/BackgroundThread.h
+++ VideoProvider/BackgroundThread.h
@@ -4,14 +4,14 @@
 
 // To start a thread I prefered to used Win32 OS function instead of using the STL, even I used some STL class (containers) in this project in different places.
 // For me, the major reseaon is because, Win32 functions for threading are much more better, Win32 is more flexible than STL thread
-// For instance it is actually not possible to suspend, resume a thread using the actual STL
-// And many other people say, that is a small wrapper around OS functions.
+// For instance it is actually not possible to suspend, resume a thread using the actual STL implementation
+// And many other people say, t<hat is a small wrapper around OS functions or POSIX thread api.
 // I don't use also a boolean flag to stop the thread, I used a sync object like the Event handle to stop a thread
-// For more details, I strongly recommends to read the Jeffrey Richter books from microsoft press. It is an old books
-// But it is really interesting to read.
+// For more details, I strongly recommends to read the Jeffrey Richter book's from microsoft press. It is an old books
+// but it is really interesting to read.
 // Just to complete my answer against the STL thread:
-// You can have many benefit using the Event handle to stop a thread, for instance, it is possible to introduce
-// a stop method with a timeout value to avoid a blocking call on the calling thread
+// You can have many benefits if you are using the Event handle to stop a thread, for instance, it is possible to introduce
+// a stop method that take a timeout argument used to avoid a blocking call on the calling thread
 // You can implement a similar pattern used in the .Net framework represented by the IAsyncResult where the behind the class which 
 // implement this interface, you will problably discovered an internal event handle object perhaps a EventWaitHandle
 
